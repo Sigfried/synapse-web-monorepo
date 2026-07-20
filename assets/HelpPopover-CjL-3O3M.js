e722a3a0ce9e247c6998be7634a5f7d499ff0d0d
Sigfried <sigfried@sigfried.org>
2026-07-20 20:43:32 +0000

    Deploying to gh-pages from @ Sigfried/synapse-web-monorepo@7b274e268cac1e581f4d745157b80a810e7c3bb7 🚀

---
+++ assets/HelpPopover-CjL-3O3M.js
@@ -0,0 +1,1 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t,t as n}from"./HelpPopover-DG0ba9ya.js";var r,i=e((()=>{t(),r=n}));export{i as n,r as t};
---
+++ assets/IconSvg-BP61r3NJ.js
@@ -0,0 +1,1 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t,r as n}from"./IconSvg-sasNO0cu.js";var r,i=e((()=>{n(),r=t}));export{i as n,r as t};
